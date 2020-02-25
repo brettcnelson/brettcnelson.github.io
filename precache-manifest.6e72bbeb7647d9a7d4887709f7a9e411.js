15c9f056036f3631f6448569830e42e537fdf235
Brett Nelson <brettcnelson@gmail.com>
2020-02-25 01:56:51 -0500

    first build

---
+++ precache-manifest.6e72bbeb7647d9a7d4887709f7a9e411.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b441dad6e02ce9f8e3de9cd4aee8db24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6134e96a05bbbdfdc00",
+    "url": "/static/css/main.a518d6e0.chunk.css"
+  },
+  {
+    "revision": "906459d5ffe865d6e3ae",
+    "url": "/static/js/2.201a218f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.201a218f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6134e96a05bbbdfdc00",
+    "url": "/static/js/main.68cf9095.chunk.js"
+  },
+  {
+    "revision": "096cd73c3a875e0e069f",
+    "url": "/static/js/runtime-main.a91cd245.js"
+  }
+]);
